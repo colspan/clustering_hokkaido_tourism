544a020be0df235f93e3ec97ccb38a534e2cf6f2
Kunihiko Miyoshi <contact@colspan.net>
2015-11-03 23:40:53 +0900

    implemented graph and datasheet of each clusters

--- mapping.js
+++ mapping.js
@@ -4,18 +4,34 @@
   var csv_files = [];
   var json_files = [];
   var tourism_stat_files = [
+    {file:"data/tourism_stat_2012.csv", label:'stat_2012'},
     {file:"data/cluster_result.csv", label:'cluster_external_2012'}
   ];
   csv_files = csv_files.concat(tourism_stat_files);
   var geodata_files = [{file:"data/hokkaido_topo.json", label:'geodata_topo'}];
   json_files = json_files.concat(geodata_files);
+  var Clusters = [
+    {index: 3, title:'孤高の都市 札幌'},
+    {index:10, title:'孤高の都市 小樽'},
+    {index: 8, title:'孤高の都市 旭川'},
+    {index: 1, title:'孤高の都市 函館'},
+    {index: 2, title:'あと一歩で孤高の都市 グループA'},
+    {index: 6, title:'あと一歩で孤高の都市 グループB'},
+    {index:11, title:'あと一歩で孤高の都市 グループC'},
+    {index: 7, title:'スキーリゾート'},
+    {index: 9, title:'北の国から'},
+    {index: 5, title:'道の駅'},
+    {index: 0, title:'観光地の狭間'},
+    {index: 4, title:'秘境'}
+  ];
 
-  // 変数初期化
-  // 最大値変数初期化
   // UI初期化
   var g,
   width = 550,
   height = 500;
+  var target_datatype = 'external';
+  // 色マップ
+  var colors = d3.scale.category20();
 
   // 読み込み実行
   var q = queue();
@@ -28,8 +44,8 @@
     q.defer(d3.csv,d.file);
     q_labels.push(d.label);
   });
-  q.awaitAll(ready);
-  function ready(error, data) {
+  q.awaitAll(main);
+  function main(error, data) {
     // 変数名解決
     var loaded={};
     for(var i=0;i<data.length;i++){
@@ -37,19 +53,38 @@
     }
     // 初期化
     var geodata = topojson.feature(loaded.geodata_topo, loaded.geodata_topo.objects.hokkaido)
-    tourism_stat_files.forEach(function(d){
-      append_tourism_stat(geodata.features,loaded[d.label],d.label);
-    });
-    visualize(geodata);
+    modify_data(geodata, loaded);
+    generate_heatmap(geodata, loaded);
+    generate_cluster_summaries(loaded);
+  }
+
+  function modify_data(geodata, loaded){
+    // 札幌市を修正
+    for(var i=0;i<geodata.features.length;i++){
+      var commune;
+      if(geodata.features[i].properties.N03_003 == '札幌市'){
+        commune = geodata.features[i].properties.N03_003;
+      }
+      else{
+        commune = geodata.features[i].properties.N03_004;
+      }
+      geodata.features[i].commune = commune;
+    }
+    // 市町村名→クラスタ番号取得
+    var output_dic = {};
+    var input = loaded.cluster_external_2012;
+    for(var i=0;i<input.length;i++){
+      output_dic[input[i].commune] = parseInt(input[i].cluster,10);
+    }
+    loaded['commune_to_cluster'] = output_dic;
   }
 
   // 描画
-  function visualize(json){
+  function generate_heatmap(json, loaded){
     var projection, path;
-    var colors = d3.scale.category20();
 
     // svg要素を作成し、データの受け皿となるg要素を追加している
-    var map = d3.select('#map').append('svg')
+    var map = d3.select('#heatmap').append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g');
@@ -68,8 +103,8 @@
     .append('path')
     .attr('d', path)
     .attr("fill", function(d){
-      if( d.cluster_external_2012 === undefined ) color = '#000';
-      else color = colors(d.cluster_external_2012);
+      if( loaded.commune_to_cluster[d.commune] === undefined ) color = '#000';
+      else color = colors(loaded.commune_to_cluster[d.commune]);
       return color;
     })
     .attr("stroke","hsl(80,100%,0%)" )
@@ -81,24 +116,153 @@
       // do nothing
     });
   }
-  function append_tourism_stat(target,input,key_name){
-    var i;
-    var output_dic = {};
-    for(i=0;i<input.length;i++){
-      output_dic[input[i].commune] = parseInt(input[i].cluster,10);
-    }
+  function generate_cluster_summaries(loaded){
+    //データ集計
+    var root_elem_id = '#cluster_summaries';
+    var root_elem = d3.select(root_elem_id);
+    var nested_data = d3.nest()
+    .key(function(d){ return d.datatype; })
+    .key(function(d){ return loaded.commune_to_cluster[d.commune]; })
+    .key(function(d){ return d.commune; })
+    .map(loaded.stat_2012);
+    var aggrigated_data = d3.nest()
+    .key(function(d){ return d.datatype; })
+    .key(function(d){ return loaded.commune_to_cluster[d.commune]; })
+    .key(function(d){ return d.commune; })
+    .rollup(function(values){ return d3.sum(values,function(d){return d.value*1000}) })
+    .map(loaded.stat_2012);
+    //クラスタごとにループ
+    var target_data = nested_data[target_datatype];
+    Clusters.forEach(function(cluster_def){
+      var cluster_index = cluster_def.index;
+      var cluster_title = cluster_def.title;
+      var container_elem = root_elem.append('div');
+      container_elem.attr('class', 'panel panel-default');
+      var cluster_elem = container_elem.append('div');
+      var elem_id = 'cluster_'+target_datatype+'_'+cluster_index;
+      cluster_elem.attr('id', elem_id);
+      cluster_elem.attr('class', 'panel-body');
+      // ヘッダ
+      var title_elem = $('<div class="panel-heading"><h2 class="panel-title">'+cluster_title+' <span class="label label-default" style="background-color:'+colors(cluster_index)+'">Cluster Number : '+cluster_index+'</span></h2></div>');
+      $('#'+elem_id).before(title_elem);
+      var communes = target_data[cluster_index];
+      //グラフ生成
+      var plot_data = [];
+      for(var commune in communes){
+        var d = communes[commune];
+        var values = [];
+        for(var i in d){
+          values.push(
+            {
+              x:parseInt(d[i].month.split('/')[1],10),
+              y:parseFloat(d[i].value)*1000
+            }
+          );
+        }
+        plot_data.push({
+          key:commune,
+          values:values
+        });
+      }
+      var chart = nv.models.lineChart().useInteractiveGuideline(true).showLegend(false);
+      chart.xAxis
+          .tickFormat(d3.format('d'));
+      chart.yAxis
+          .tickFormat(d3.format(',d'));
+      var chart_elem = cluster_elem.append('svg')
+          .attr('height','300px');
+      chart_elem.datum(plot_data)
+          .transition().duration(500)
+          .call(chart);
+      nv.utils.windowResize(chart.update);
 
-    // join
-    for(i=0;i<target.length;i++){
-      var commune;
-      if(target[i].properties.N03_003 == '札幌市'){
-        commune = target[i].properties.N03_003;
+      // 内訳表
+      var comma_formatter = d3.format(",d");
+      var target_columns = [
+        /*{
+          title: '振興局',
+          field: 'commune',
+          formatter: function(v,r,i){
+            return _this.consts.PROMOTION_BUREAU_DIC[v];
+          },
+          sorter: function(a,b){
+            var dic = _this.consts.PROMOTION_BUREAU_DIC;
+            return dic[a].toString() > dic[b].toString() ? 1 : -1;
+          },
+          sortable :true
+        },*/
+        {
+          field: 'commune',
+          title: '市町村名'
+        },
+        {
+          field: 'external',
+          title: '道外',
+          align: 'right',
+          formatter: comma_formatter,
+          sortable: true
+        },
+        {
+          field: 'local',
+          title: '道内',
+          align: 'right',
+          formatter: comma_formatter,
+          sortable: true
+        },
+        {
+          field: 'day_trip',
+          title: '日帰り',
+          align: 'right',
+          formatter: comma_formatter,
+          sortable: true
+        },
+        {
+          field: 'stay',
+          title: '宿泊(国内)',
+          align: 'right',
+          formatter: comma_formatter,
+          sortable: true
+        },
+        {
+          field: 'sum',
+          title: '観光客合計',
+          formatter: comma_formatter,
+          align: 'right',
+          sortable: true
+        },
+        {
+          field: 'length_stay',
+          title: '宿泊延日数(国内)',
+          align: 'right',
+          formatter: comma_formatter,
+          sortable: true
+        }
+      ];
+      var target_types = ['external','local','day_trip','stay','length_stay','sum'];
+
+      var table_data = [];
+      for(var commune in communes){
+        var record = {};
+        record.commune = commune;
+        target_types.forEach(function(target_type){
+          record[target_type] = aggrigated_data[target_type][cluster_index][commune];
+        });
+        table_data.push(record);
       }
-      else{
-        commune = target[i].properties.N03_004;
-      }
-      target[i][key_name] = output_dic[commune];
-    }
+      var data = table_data.sort(function(a, b) {
+        return d3.descending(a[target_datatype], b[target_datatype]);
+      });
+      var table_elem = $('<table>');
+      table_elem.appendTo($('#'+elem_id));
+      table_elem.bootstrapTable({
+        height: 350,
+        striped: false,
+        pagination: false,
+        showColumns: true,
+        minimumCountColumns: 2,
+        columns: target_columns,
+        data: data
+      });
+    });
   }
-
 })();
